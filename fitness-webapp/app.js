48b2f9d29362f833d8f7e0e02e267aba910c9868
unknown <Gnutz>
2019-09-23 12:05:51 +0200

    add passport config

--- fitness-webapp/app.js
+++ fitness-webapp/app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const passport = require('passport');
+require('./app_server/models/db');
+require('./config/passport');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
@@ -20,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
---
+++ fitness-webapp/config/passport.js
@@ -0,0 +1,38 @@
+// CONFIGURATION FROM SLIDES TAKES HASHING INTO ACCOUNT:
+var passport = require('passport');
+var mongoose = require('mongoose');
+
+var User = mongoose.model('User');
+
+passport.use(new LocalStrategy({
+    usernameField: 'username'
+    },
+    function(username, password, done) {
+        User.findOne({ username: username}, function(err, user) {
+            if(err) { return done(err); }
+            if(!user) {return done(null, false, {
+                message: 'Incorrect username.'
+            });
+        }
+        if(user.passport != password) {
+            return done(null, false, {
+                message: 'Incorrect password.'
+            });
+        }
+        return done(null, user);
+    });  })
+);
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+  db.users.findById(id, function (err, user) {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+
+
