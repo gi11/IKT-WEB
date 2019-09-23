f43262da0f151c90d9bab338d6a43f931c52f593
unknown <Gnutz>
2019-09-23 12:16:22 +0200

    mod index route

--- fitness-webapp/app_server/routes/index.js
+++ fitness-webapp/app_server/routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const ctrlHome = require('../controllers/HomeController')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', ctrlHome.index);
 
 module.exports = router;
