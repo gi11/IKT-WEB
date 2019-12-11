427bce977b7876d5844becf2888397aece5ccf4a
kasper andersen <kga@live.dk>
2019-12-11 17:39:41 +0100

    mongodb handles score

--- dual-n-back-react/server/server.js
+++ dual-n-back-react/server/server.js
@@ -31,24 +31,29 @@ const server = http.createServer(app);
 
 const io = socketIO(server);
 
-let highScores = [
-  { "score": 0, "name": "none" },
-  { "score": 0, "name": "none" },
-  { "score": 0, "name": "none" },
-  { "score": 0, "name": "none" },
-  { "score": 0, "name": "none" },
-  { "score": 0, "name": "none" },
-  { "score": 0, "name": "none" },
-  { "score": 0, "name": "none" },
-  { "score": 0, "name": "none" },
-  { "score": 10000, "name": "BEST" }
-];
+// let highScores = [
+//   { "score": 0, "name": "none" },
+//   { "score": 0, "name": "none" },
+//   { "score": 0, "name": "none" },
+//   { "score": 0, "name": "none" },
+//   { "score": 0, "name": "none" },
+//   { "score": 0, "name": "none" },
+//   { "score": 0, "name": "none" },
+//   { "score": 0, "name": "none" },
+//   { "score": 0, "name": "none" },
+//   { "score": 10000, "name": "BEST" }
+// ];
 
 
 // This is what the socket.io syntax is like, we will work this later
 io.on('connection', socket => {
   //   highScores.sort((a, b) => (a.score < b.score) ? 1 : -1);
   console.log('New client connected')
+
+   // disconnect is fired when a client leaves the server
+   socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
   //   io.sockets.emit("scores updated", highScores)
 
   //   socket.on("new score", (score) => {
@@ -79,10 +84,6 @@ io.on('connection', socket => {
     emitHighestScores();
   })
 
-  // disconnect is fired when a client leaves the server
-  socket.on('disconnect', () => {
-    console.log('user disconnected')
-  })
 })
 
 function emitHighestScores() {
